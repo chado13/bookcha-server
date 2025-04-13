13e89a7000e8abd7f316c28c6bd579011be02575
chado13 <geaya13@gmail.com>
2025-04-13 12:51:18 +0900

    chore: table 변경

--- app/orm/tables.py
+++ app/orm/tables.py
@@ -5,12 +5,13 @@ import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.orm.base import Base, TimestampBase
+import pytz
 
 
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True)
     public_id: Mapped[UUID] = mapped_column(
         sa.UUID(as_uuid=True),
         default=uuid4,
@@ -21,18 +22,25 @@ class User(Base):
     last_login: Mapped[datetime.datetime | None] = mapped_column(
         sa.TIMESTAMP(timezone=True), nullable=True
     )
-    is_superuser: Mapped[bool] = mapped_column(sa.Boolean, default=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        sa.Boolean, default=False, server_default="false"
+    )
     nickname: Mapped[str] = mapped_column(sa.String(12), unique=True)
     email: Mapped[str] = mapped_column(sa.String(255), unique=True)
     photo: Mapped[str] = mapped_column(sa.String(200), default="")
     intro: Mapped[str] = mapped_column(sa.Text, default="")
     is_active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.func.now()
+    )
 
 
 class Record(TimestampBase):
-    __tablename__="record"
+    __tablename__ = "record"
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-    user_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
     title: Mapped[str] = mapped_column(sa.String(100), nullable=False)
     author: Mapped[str] = mapped_column(sa.String(50), nullable=False)
     evaluation: Mapped[int] = mapped_column(sa.Integer, nullable=True)
@@ -41,6 +49,12 @@ class Record(TimestampBase):
 
     __table_args__ = (
         sa.Index("ix_record_id", "id"),
-        sa.Index("ix_recode_user_author_title","user_id","author","title", unique=True),
-        sa.Index("ix_record_updated_at", "updated_at")
+        sa.Index(
+            "ix_recode_user_author_title",
+            "user_id",
+            "author",
+            "title",
+            unique=True,
+        ),
+        sa.Index("ix_record_updated_at", "updated_at"),
     )
